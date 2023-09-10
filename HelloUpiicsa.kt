cd4c37c4e31e8b89487578ab61579196c532aa55
Rogelio Rafael Gonzalez Martinez <144263884+R3TR029@users.noreply.github.com>
2023-09-10 12:27:03 -0600

    Update HelloUpiicsa.kt

--- HelloUpiicsa.kt
+++ HelloUpiicsa.kt
@@ -2,15 +2,23 @@
 
 fun main() {
     
-    /*El "println" hace dar un salto de linea sin la necesidada de ocupar "\n"*/
-    println("Helloooo, UPIICSA!!!")
+     /*1- Imprimir el mismo mensaje que imprimimos en la practica "HelloUPIICSA"*/
     
-    /*El "print" lo que hace es una linea de texto pero sin dar salto a otro parrafo, para esto se necesita un "\n"*/
-   
     /*Sin*/ print("Helloooo, UPIICSA!!!")
     
-    /*Con*/ print("Helloooo, UPIICSA!!!\n")
+    /*2- Abajo de la linea de código de la tarea 
+	Poner otro mensaje en consola ¿Qué pasa con los dos mensajes?*/
+   
+    /*Con \n*/ print(", Este mensaje se juntara ya que no tiene salto de linea en el anterior print.\n")
     
-    print("Helloooo, UPIICSA!!!")
+    
+    /*3- Ahora utiliza println e imprime alguna otra cosa, ¿Qué diferencia hay con print?*/
+    
+    /**/
+    
+    println("¿Que pasa con los dos mensajes?")
+   
+    print("- El 'print' lo que hace es una linea de texto pero sin dar salto a otro parrafo, para esto se necesita un '\\n'.")
+    print("\n- El 'println' hace dar un salto de linea sin la necesidad de ocupar '\\n'.")
     
 }
