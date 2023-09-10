3fe019eecaa96599cf583e3b3ff0a5cb276a26b2
Rogelio Rafael Gonzalez Martinez <144263884+R3TR029@users.noreply.github.com>
2023-09-10 11:32:06 -0600

    Create HelloUpiicsa.kt

---
+++ HelloUpiicsa.kt
@@ -0,0 +1,16 @@
+/*Rogelio Rafael Gonzalez Martinez 6NM61*/
+
+fun main() {
+    
+    /*El "println" hace dar un salto de linea sin la necesidada de ocupar "\n"*/
+    println("Helloooo, UPIICSA!!!")
+    
+    /*El "print" lo que hace es una linea de texto pero sin dar salto a otro parrafo, para esto se necesita un "\n"*/
+   
+    /*Sin*/ print("Helloooo, UPIICSA!!!")
+    
+    /*Con*/ print("Helloooo, UPIICSA!!!\n")
+    
+    print("Helloooo, UPIICSA!!!")
+    
+}
