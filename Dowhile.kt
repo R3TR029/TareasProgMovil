c4bf77b946d20db0d43636af94cee0b8a1a0f322
Rogelio Rafael Gonzalez Martinez <144263884+R3TR029@users.noreply.github.com>
2023-10-03 11:47:20 -0600

    Create Dowhile.kt

---
+++ Dowhile.kt
@@ -0,0 +1,10 @@
+/*Rogelio Rafael Gonzalez Martinez 6NM61*/
+
+fun main (){
+    var contador = 0
+    do {
+        println("Contador: $contador")
+        contador++
+    } while (contador < 5)
+}
+
