93c7abfc95cdfe00e0c22fa68c9f10cd5514dd07
Rogelio Rafael Gonzalez Martinez <144263884+R3TR029@users.noreply.github.com>
2023-10-03 11:35:11 -0600

    Create arearectangulo.kt

---
+++ arearectangulo.kt
@@ -0,0 +1,12 @@
+/*Rogelio Rafael Gonzalez Martinez 6NM61*/
+
+fun areaRectangulo(){
+    val base = 15f
+    val altura = 3f
+    val area = areaRectangulo(base,altura)
+    println("El area del rectangulo es $area")
+}
+
+fun areaRectangulo(base: Float, altura: Float):Float{
+    return base*altura
+}
