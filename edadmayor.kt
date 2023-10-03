aef53bcb74a120d06a2aa022cb6a6d6f2aa88725
Rogelio Rafael Gonzalez Martinez <144263884+R3TR029@users.noreply.github.com>
2023-10-03 12:07:26 -0600

    Update and rename edadmayor.kt to edad.kt

--- edadmayor.kt
+++
@@ -1,7 +0,0 @@
-fun main(){
-    print("Ingresa edad y presiona enter (escribe solo el numero ): ")
-    val age = readLine()?.toInt()
-    if (age == 18){
-        println("Ya eres mayor de edad!")
-    }
-}
