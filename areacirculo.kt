62edbf55784b0541c763ac948502b2ac8740d045
Rogelio Rafael Gonzalez Martinez <144263884+R3TR029@users.noreply.github.com>
2023-10-03 11:39:18 -0600

    Create areacirculo.kt

---
+++ areacirculo.kt
@@ -0,0 +1,12 @@
+/*Rogelio Rafael Gonzalez Martinez 6NM61*/
+
+import kotlin.math.PI
+
+fun main() {
+    
+    val radio = 6.0
+    val area = PI * radio * radio
+
+    
+    println("El área del círculo con radio $radio es: $area")
+}
