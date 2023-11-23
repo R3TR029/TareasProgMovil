1ae4d83fad46c94bf1bfec90a572068da455710c
Rogelio Rafael Gonzalez Martinez <144263884+R3TR029@users.noreply.github.com>
2023-11-22 20:20:07 -0600

    Delete Segundo Parcial/Viajes directory

--- Segundo Parcial/Viajes/src/main/kotlin/Clases/Movie.kt
+++
@@ -1,7 +0,0 @@
-package Clases
-
-data class Movie(
-    val  name: String, val gender: String, val  duration: Double) {
-    val scaryMovie = Movie ("Scary Movie", "Comedia",83.80 )
-    val createdAt =""
-}
